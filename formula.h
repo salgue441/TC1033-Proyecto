2819d9ee3f5151e496e82b3618f5d978142b6c98
Carlos Salguero <75867326+salgue441@users.noreply.github.com>
2021-11-21 21:55:17 -0600

    Update formula.h

--- formula.h
+++ formula.h
@@ -1,128 +1,78 @@
-#include <iostream>
+#include <string>
+#include "vehiculo.h"
 
-class Formula
-{   
+class Formula : public Vehiculo
+{  
 private:
-    // variables de instancia
-    std::string modelo_vehiculo, nombre_del_piloto[50], nombre_escuderia[50];
-    int num_llantas, asientos_disp, caracteres_escuderia, caracteres_piloto, num_de_vehiculo;
     float peso_vehiculo;
+    std::string nombre_escuderia[50], nombre_piloto[50];
+    int caracteres_escuderia, caracteres_piloto;
 
 public:
-    /* ---- Constructores ---- */
-    // Constructor default
-    Formula() : modelo_vehiculo(""), num_llantas(4), asientos_disp(1), peso_vehiculo(752), num_de_vehiculo(1)
-    {
-        // para nombre del piloto
-        for (int i = 0; i < 50; i++)
-            nombre_del_piloto[i] = "";
-
-        // para nombre de escuderia
+    /* ---- Constructor ---- */
+    Formula(std::string nombre, int llantas, int asientos, int piloto, int identificador) : 
+        Vehiculo(nombre, llantas, asientos, piloto, identificador) { 
         for (int i = 0; i < 50; i++)
             nombre_escuderia[i] = "";
-     };
 
-    // constructor con parámetros
-    Formula(std::string nombre, int numero_llantas, int numero_asientos, float peso, int numero) :
-            modelo_vehiculo(nombre), num_llantas(numero_llantas), 
-            asientos_disp(numero_asientos), peso_vehiculo(peso), num_de_vehiculo(numero)
-    {
-        // para nombre del piloto
         for (int i = 0; i < 50; i++)
-            nombre_del_piloto[i] = "";
-
-        // para nombre de escuderia
-        for (int i = 0; i < 50; i++)
-            nombre_escuderia[i] = "";
+            nombre_piloto[i] = "";
     };
 
-    /* ---- Getter y setters ---- */
-    // para nombre_vehiculo
-    void set_nombre(std::string );
-    std::string get_nombre();
-
-    // para num_llantas
-    void set_llantas(int );
-    int get_llantas();
-
-    // para asientos_disp
-    void set_asientos(int );
-    int get_asientos();
-
-    // para peso
-    void set_peso_auto(float );
-    float get_peso_auto();
-
-    // para numero de vehiculo
-    void set_numero_auto(int );
-    int get_numero_auto();
-
-    /* ---- Funciones ---- */
-    float calcula_peso_con_piloto_y_gasolina(float, float, float);
+    /* ---- funciones ---- */
+    void calcula_peso_con_piloto_y_gasolina(float , float , float);
     void agrega_escuderia(std::string );
     void agrega_piloto(std::string );
-    bool verifica_piloto(std::string );
     bool verifica_escuderia(std::string );
+    bool verifica_piloto(std::string );
 };
 
-/* ---- Llenando los getters y setter ---- */ 
-// getter y setter para nombre del vehículo
-std::string Formula::get_nombre() { return modelo_vehiculo; }
-void Formula::set_nombre(std::string nombre) { modelo_vehiculo = nombre; }
-
-// getter y setter para llantas
-int Formula::get_llantas() { return num_llantas; }
-void Formula::set_llantas(int numero_llantas) { num_llantas = numero_llantas; }
-
-// getter y setter para asientos
-int Formula::get_asientos() { return asientos_disp; }
-void Formula::set_asientos(int numero_asientos) { asientos_disp = numero_asientos; }
-
-// getter y setter para peso
-float Formula::get_peso_auto() { return peso_vehiculo; }
-void Formula::set_peso_auto(float peso) { peso_vehiculo = peso; }
-
-// getter y setter para num_de_vehiculo
-int Formula::get_numero_auto() { return num_de_vehiculo; }
-void Formula::set_numero_auto(int numero) { num_de_vehiculo = numero; }
-
-/* ---- Llenado de funciones ---- */
-/* Función que calcula el peso total del vehículo con el piloto + gasolinas y retorna si el 
-peso total excede el límite o no.
-parámetros:
-    - float peso del vehículo
-    - float peso del piloto
-    - float peso de la gasolina
-*/
-float Formula::calcula_peso_con_piloto_y_gasolina(float peso, float peso_piloto, float peso_gas)
+/* ----  Llenando funciones ---- */
+/* Función que calcula si el peso total del vehículo excede los límites establecidos por la FIA. 
+Se crean dos variables adicionales llamadas peso_lim que es limite de peso maximo establecido por la FIA,
+peso_total que es la suma de los pesos ingresados por el usuario y peso_diferencia que calcula 
+la diferencia entre el peso total y el peso limite. Al final, la función muestra en la terminal
+si el peso excede el limite o no y por que cantidad lo excede o queda bajo el rango.
+params:
+    - peso de tipo float, peso del vehiculo - normalmente un vehiculo de f1 pesa 752 kgs.
+    - peso_piloto de tipo float, peso del piloto - lo establecido por la FIA es de 80 kgs.
+    - peso_gas de tipo float, peso de la gasolina en el vehiculo - establecido por la fia son 110 kgs
+*/ 
+void Formula::calcula_peso_con_piloto_y_gasolina(float peso, float peso_piloto, float peso_gas)
 {
-    std::string mensaje = " ";
-    float peso_total = peso + peso_piloto + peso_gas;
+    float peso_total = peso + peso_piloto + peso_gas, peso_lim = 942, peso_diferencia;
+    std::string mensaje = "";
 
-    if (peso_total > 942) mensaje = " excede el límite establecido";
-    else mensaje = " está dentro del límite establecido";
+    peso_diferencia = peso_total - peso_lim;
 
-    std::cout << "El vehículo ingresado tiene un peso que" << mensaje << "; su peso total es: ";
-    return peso_total;
+    if (peso_total > peso_lim) mensaje = "El peso excede el límite con: ";
+    else mensaje = "El peso está dentro del rango con: ";
+
+    std::cout << mensaje << peso_diferencia << " kgs de diferencia" << std::endl;
 }
 
-/* Función que agrega el nombre de la escuderia pero no la muestra en pantalla para el usuario
-parámetro:
-    - std::string escuderia: el usuario ingresa el nombre de la escuderia en formato de string y esta
-        función lo agrega
+/* Función que agrega el nombre de la escuderia a la que pertenece el objeto. No devuelve nada ni 
+muestra nada al usuario. 
+params:
+    - nombre de la escuderia (equipo) en tipo de caracteres 
+        - escuderias disponibles: Ferrari, Aston Martin, McLaren Racing, Red Bull, Alfa Romtero Racing, 
+                Alpha Tauri, Mercedes AMG F1, Alpine F1 Team, Haas F1 Team
 */
 void Formula::agrega_escuderia(std::string escuderia)
 {
     if (caracteres_escuderia < 50)
     {
         nombre_escuderia[caracteres_escuderia] = escuderia;
-        caracteres_escuderia++; // incrementando el contador
+        caracteres_escuderia++;
     }
 }
 
-/* Función que verifica si la escudería ingresada se encuentra en la cadena de texto
-parámetros:
-    - escuderia: recibe el nombre de una escuderia de F1 y comprueba si esta asignada a un objeto
+/* Función que verifica si la escuderia ingresada por el usuario se encuentra entre los atributos
+del objeto 
+params:
+    - nombre de la escuderia (equipo) en tipo de caracteres 
+        - escuderias disponibles: Ferrari, Aston Martin, McLaren Racing, Red Bull, Alfa Romtero Racing, 
+                Alpha Tauri, Mercedes AMG F1, Alpine F1 Team, Haas F1 Team
 */
 bool Formula::verifica_escuderia(std::string escuderia)
 {
@@ -133,28 +83,31 @@ bool Formula::verifica_escuderia(std::string escuderia)
     return false;
 }
 
-/* Función que agrega el nombre del piloto de F1 pero no la muestra en pantalla para el usuario
-parámetro:
-    - std::string piloto: el usuario ingresa el nombre del piloto en formato de string y esta función lo agrega
+/* Función que agrega el nombre del piloto a la que pertenece el objeto. No devuelve nada ni 
+muestra nada al usuario. 
+params:
+    - nombre del piloto de tipo string (caracteres)
+
 */
 void Formula::agrega_piloto(std::string piloto)
 {
     if (caracteres_piloto < 50)
     {
-        nombre_del_piloto[caracteres_piloto] = piloto;
+        nombre_piloto[caracteres_piloto] = piloto;
         caracteres_piloto++;
     }
 }
 
-/* Función que comprueba si el nombre del piloto ingresado pertenece al objeto 
-parámetros: 
-    - piloto: recibe el nombre del piloto de F1 y comprueba si esta asignado al objeto
+/* Función que verifica si el nombre del piloto ingresado por el usuario se encuentra 
+entre los atributos del objeto 
+params:
+    - nombre del piloto
 */
 bool Formula::verifica_piloto(std::string piloto)
 {
-    piloto = nombre_del_piloto[0];
+    piloto = nombre_piloto[0];
     for (int i = 1; i < 49; i++)
-        if (nombre_del_piloto[i] == piloto)
+        if (nombre_piloto[i] == piloto)
             return true;
     return false;
 }
