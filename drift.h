6f3d2005536f1dd94a59e0a6029aa386bc50d9a1
Carlos Salguero <75867326+salgue441@users.noreply.github.com>
2021-11-21 21:55:03 -0600

    Update drift.h

--- drift.h
+++ drift.h
@@ -1,76 +1,34 @@
 #include <iostream>
+#include "vehiculo.h"
 
-class Drift
+class Drift : public Vehiculo
 {
-private:
-    int num_llantas, num_asientos, num_pilotos;
-    std::string modelo_de_auto;
-
 public:
-    /* Constructores */
-    Drift() : modelo_de_auto("None"), num_llantas(4), num_asientos(2), num_pilotos(2) { }; // default
-    Drift(std::string modelo, int llantas, int asientos, int pilotos) : 
-        modelo_de_auto(modelo), num_llantas(llantas), num_asientos(asientos), num_pilotos(pilotos) { }; // custom constructor
-
-    /* getters y setters */
-    // para el modelo
-    void set_modelo(std::string );
-    std::string get_modelo();
-
-    // para llantas
-    void set_numLlantas(int );
-    int get_numLlantas();
-
-    // para asientos
-    void set_asientos(int );
-    int get_asientos();
-
-    // para num_pilotos
-    void set_pilotos(int );
-    int get_pilotos();
-
-    // función
+    /* ---- Constructor ---- */
+    Drift(std::string nombre, int llantas, int asientos, int piloto, int identificador) : 
+        Vehiculo(nombre, llantas, asientos, piloto, identificador) { };
+    
+    /* ---- Funciones ---- */
     std::string es_apto_para_drift(std::string );
 };
 
-/* Llenando getters y setters */
-// para modelo
-std::string Drift::get_modelo() { return modelo_de_auto; }
-void Drift::set_modelo(std::string modelo) { modelo_de_auto = modelo; }
-
-// para llantas
-int Drift::get_numLlantas() { return num_llantas; }
-void Drift::set_numLlantas(int llantas) { num_llantas = llantas; }
-
-// para asientos
-int Drift::get_asientos() { return num_asientos; }
-void Drift::set_asientos(int asientos) { num_asientos = asientos; }
-
-// para pilotos
-int Drift::get_pilotos() { return num_pilotos; }
-void Drift::set_pilotos(int pilotos) { num_pilotos = pilotos; }
-
-/* Función que comprueba si el vehículo ingresado es apto para hacer drift. 
-Params: 
-    - cuatro_por_cuatro de tipo booleano
-    - trasmicion de tipo booleano
-*/
-std::string Drift::es_apto_para_drift(std::string string_opcion)
+std::string Drift::es_apto_para_drift(std::string opcion)
 {
-    std::string result;
-    int op; 
+    std::string result; 
+    int op;
 
-    std::cout << "Selecciona el tipo de trasmición: 1. Manual \t2. Automático :";
+    std::cout << "Seleccione el tipo de trasmición: 1. Manual \t2. Automático: ";
     std::cin >> op;
-    std::cout << "[parámetro] El vehículo es 4 x 4?" << std::endl;
+
+    std::cout << "[parametro] El vehiculo es 4 x 4?" << std::endl;
 
     // condicionales
-    if (op == 1 && (string_opcion == "si" || string_opcion == "Si")) 
+    if (op == 1 && (opcion == "si" || opcion == "Si"))
         result = "El carro es apto para hacer drift!";
-    else if (op == 2 && (string_opcion == "si" || string_opcion == "Si"))
+    else if (op == 2 && (opcion == "si" || opcion == "Si"))
         result = "El carro puede ser apto para hacer drift pero no es recomendable";
-    else    
-        result = "El carro no es apto para hacer drift";
-
+    else 
+        result = "El carro no es pato para hacer drift";
+    
     return result;
 }
