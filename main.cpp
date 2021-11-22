0b28c524f48291d8f5cc31a4817fa6dc3ff27054
Carlos Salguero <75867326+salgue441@users.noreply.github.com>
2021-11-21 21:56:26 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,54 +5,50 @@
 
 int main()
 {
-    /* ---- objeto de clase formula ---- */
-    std::cout << "---- Objeto de clase F1 ----" << std::endl;
-    Formula f1("Ascari KZ1", 4, 1, 752, 95); // objetos con valores de entrada
-    // mostrando los valores
-    std::cout << "Datos del objeto: \n" << "Modelo del vehículo: " << f1.get_nombre() << "\n"
-                  << "Número de llantas: " << f1.get_llantas() << "\n"
-                  << "Número de asientos: " << f1.get_asientos() << "\n" 
-                  << "Peso del vehículo: " << f1.get_peso_auto() << "\n" 
-                  << "Número identificador: " << f1.get_numero_auto() << std::endl << std::endl;
-    
-    // verifica el peso total del vehículo ingresado
-    std::cout << "El peso del vehículo tiene que ser menor o igual a 942 kilogramos" << std::endl;
-    std::cout << f1.calcula_peso_con_piloto_y_gasolina(752, 100, 80) << std::endl;
+    /* ---- Objeto de clase fórmula ---- */
+    Formula f1("Askari KZ1", 4, 1, 1, 120);
+    std::cout << "Datos del objeto: \n" << "Modelo del vehículo: " << f1.get_modelo() << "\n" 
+              << "Número de llantas: " << f1.get_llantas() << "\n" 
+              << "Número de asientos: " << f1.get_asientos() << "\n"
+              << "Número de pilotos: " << f1.get_piloto() << "\n" 
+              << "Número identificador: " << f1.get_id() << std::endl;
 
-    // agrega nombre de la escuderia y lo verifica
-    f1.agrega_escuderia("McLaren"); 
-    std::cout << "La escudería ingresada se encuentra (Si = 1, No = 0): " 
-              << f1.verifica_escuderia("McLaren") << std::endl;
+    f1.calcula_peso_con_piloto_y_gasolina(752, 80, 110); 
 
-    // agrega nombre del piloto y lo verifica
+    // agregando y verificando el nombre de la escudería
+    f1.agrega_escuderia("Mercedes AMG F1");
+    std::cout << "La escudería ingresada fue encontrada (Si = 1, No = 0): " 
+              << f1.verifica_escuderia("Mercedes AMG F1") << std::endl;
+
+    // agregando y verificando el nombre del piloto
     f1.agrega_piloto("Lewis Hamilton");
-    std::cout << "El piloto ingresado se encunetra (Si = 1, No = 0): "
+    std::cout << "El nombre del piloto ingresado fue encontrado (Si = 1, No = 0): "
               << f1.verifica_piloto("Lewis Hamilton") << std::endl;
 
     /* ---- objeto de clase drift ---- */
     std::cout << "\n---- Objeto de clase Drift ----" << std::endl;
     Drift carro_drift1("BMW M2 Competition", 4, 2, 2, 120); // objeto con valores de entrada
     std::cout << "Datos del objeto: \n" << "Modelo del vehículo: " << carro_drift1.get_modelo() << "\n" 
-              << "Número de llantas: " << carro_drift1.get_numLlantas() << "\n"
+              << "Número de llantas: " << carro_drift1.get_llantas() << "\n"
               << "Número de asientos disponibles: " << carro_drift1.get_asientos() << "\n"
-              << "Número de pilotos: " << carro_drift1.get_pilotos() << std::endl << std::endl;
+              << "Número de pilotos: " << carro_drift1.get_piloto() 
+              << "Número identificador: " << carro_drift1.get_id() << std::endl << std::endl;
 
     // condicional que verifica si el vehículo es apto para hacer drift
-    std::cout << carro_drift1.es_apto_para_drift("si");
-
+    std::cout << carro_drift1.es_apto_para_drift("si") << std::endl;
+    
     /* ---- objeto de clase rally ---- */
     std::cout << "\n\n---- Objeto de clase Rally ----" << std::endl;
-    Rally carro_rally("Citroën C4 WRC", 4, 134, 1, 1230);
+    Rally carro_rally("Citroën C4 WRC", 4, 2, 1, 95, 100, 1000);
     std::cout << "Datos del objeto: \n" << "Modelo del vehículo: " << carro_rally.get_modelo() << "\n"
               << "Número de llantas: " << carro_rally.get_llantas() << "\n"
-              << "Número identificador del vehículo: " << carro_rally.get_numero() << "\n" 
-              << "Número de pilotos: " << carro_rally.get_pilotos() << "\n" 
+              << "Número identificador del vehículo: " << carro_rally.get_id() << "\n" 
+              << "Número de pilotos: " << carro_rally.get_piloto() << "\n" 
               << "Peso del vehículo: " << carro_rally.get_peso() << " kg" << std::endl;
 
     // tipo de campeonato
     carro_rally.campeonato_rally("Junior");
-    std::cout << "\n Si = 1, No = 0:  " << carro_rally.verifica_campeonato("Junior") << std::endl;
-
+    std::cout << "\nEl campeonato fue encontrado (Si = 1, No = 0):  " << carro_rally.verifica_campeonato("Junior") << std::endl;
     
     return 0;
 }
