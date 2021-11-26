5f6a8cf46b40d96adb30e82880a40770ec83d220
Carlos Salguero <75867326+salgue441@users.noreply.github.com>
2021-11-25 19:29:52 -0600

    Update drift.h

--- drift.h
+++ drift.h
@@ -1,34 +1,50 @@
-#include <iostream>
+#ifndef DRIFT_H
+#define DRIFT_H
+
 #include "vehiculo.h"
 
 class Drift : public Vehiculo
 {
+private:
+    std::string trasmicion_vehiculo;
+
 public:
     /* ---- Constructor ---- */
-    Drift(std::string nombre, int llantas, int asientos, int piloto, int identificador) : 
-        Vehiculo(nombre, llantas, asientos, piloto, identificador) { };
-    
+    Drift(std::string nombre, int llantas, int asientos, int pilotos, int ID, float peso, 
+        std::string trasmicion) :
+        Vehiculo(nombre, llantas, asientos, pilotos, ID, peso), trasmicion_vehiculo(trasmicion) {};
+
+    /* ---- getter y setter ---- */
+    std::string get_trasmicion();
+    void set_trasmicion(std::string );
+
     /* ---- Funciones ---- */
     std::string es_apto_para_drift(std::string );
 };
 
-std::string Drift::es_apto_para_drift(std::string opcion)
+/* ---- Llenado de la función ---- */
+std::string Drift::get_trasmicion() { return trasmicion_vehiculo; }
+void Drift::set_trasmicion(std::string trasmicion) { trasmicion_vehiculo = trasmicion; }
+
+/* ---- Funciones ---- */
+std::string Drift::es_apto_para_drift(std::string trasmicion)
 {
-    std::string result; 
+    std::string result = "";
     int op;
 
-    std::cout << "Seleccione el tipo de trasmición: 1. Manual \t2. Automático: ";
-    std::cin >> op;
+    std::cout << "Tipo de trasmición: " << trasmicion << std::endl;
+    std::cout << "El vehículo es 4 x 4? \t1. Si \t2. No: ";
+    std::cin >> op; 
 
-    std::cout << "[parametro] El vehiculo es 4 x 4?" << std::endl;
+    // condicional
+    if (op == 1 && (trasmicion == "Manual" || trasmicion == "manual"))
+        result = "El carro es para hacer drift!";
+    else if (op == 2 && (trasmicion == "Manual" || trasmicion == "manual"))
+        result = "El carro puede ser apto para hacer drift pero no es recomendado";
+    else
+        result = "El carro no es apto para hacer drift";
 
-    // condicionales
-    if (op == 1 && (opcion == "si" || opcion == "Si"))
-        result = "El carro es apto para hacer drift!";
-    else if (op == 2 && (opcion == "si" || opcion == "Si"))
-        result = "El carro puede ser apto para hacer drift pero no es recomendable";
-    else 
-        result = "El carro no es pato para hacer drift";
-    
     return result;
 }
+
+#endif // !DRIFT_H
